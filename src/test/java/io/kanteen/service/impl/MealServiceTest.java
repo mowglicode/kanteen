d84f00743c02dc4f0b7bb75a83869c64faa0bbbe
SimonDry <simon.deleruyelle@gmail.com>
2018-08-30 09:42:09 +0200

    test merge wentz

--- src/test/java/io/kanteen/service/impl/MealServiceTest.java
+++ src/test/java/io/kanteen/service/impl/MealServiceTest.java
@@ -12,10 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
 
 import java.util.Date;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest

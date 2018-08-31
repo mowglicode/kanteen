6ce9d912e64814e7f354a74b5772b65c082b9876
ArnaudDesfontaines <desfontainesarnaud@gmail.com>
2018-08-31 12:00:31 +0200

    All ParentService Tests are working even with two children

--- src/main/java/io/kanteen/service/IAccountService.java
+++ src/main/java/io/kanteen/service/IAccountService.java
@@ -6,8 +6,14 @@ import io.kanteen.persistance.entity.Account;
 import java.util.List;
 
 public interface IAccountService {
+
+    Account saveAccount(Account account);
+
     AccountDto saveAccount(AccountDto accountDto);
+
     List<AccountDto> getAllAccounts();
+
     AccountDto getAccountById(long id);
+
     void deleteAccount(long id);
 }

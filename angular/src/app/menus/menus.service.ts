984823ac6a476ac4121fece10d9b0623250ebda5
SambaNiang31 <esambaniang@gmail.com>
2018-08-31 15:18:56 +0200

    Merge angular and CORS

---
+++ angular/src/app/menus/menus.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+
+export type Menu = {
+  content: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenusService {
+  menus: Menu[]=[];
+
+
+  constructor(public http:HttpClient) {}
+
+  getAllMenus(){
+    this.http.get('http://localhost:8585/api/menus')
+      .subscribe((r:any[]) => {
+      this.menus =r
+      console.log(this.menus);
+    });
+  }
+
+}

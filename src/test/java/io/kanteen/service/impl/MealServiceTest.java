84cf07de96e2672a2550d2f04514e406ac366e41
SimonDry <simon.deleruyelle@gmail.com>
2018-08-31 14:44:01 +0200

    incomplete : tests in progress for recap by parent

--- src/test/java/io/kanteen/service/impl/MealServiceTest.java
+++ src/test/java/io/kanteen/service/impl/MealServiceTest.java
@@ -2,6 +2,10 @@ package io.kanteen.service.impl;
 
 import io.kanteen.dto.ChildDto;
 import io.kanteen.dto.MealDto;
+import io.kanteen.dto.ParentDtoFull;
+import io.kanteen.dto.ParentDtoLight;
+import io.kanteen.persistance.entity.Parent;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
@@ -25,6 +29,8 @@ public class MealServiceTest {
     MealService mealService;
     @Autowired
     ChildService childService;
+    @Autowired
+    ParentService parentService;
 
     @Autowired
     ModelMapper modelMapper;
@@ -33,6 +39,8 @@ public class MealServiceTest {
     ChildDto childDto2;
     ChildDto childAvecId;
     ChildDto childAvecId2;
+    ParentDtoFull parentDto;
+    ParentDtoFull parentDtoAvecId;
     String dateString;
 
     @org.junit.Before
@@ -40,7 +48,10 @@ public class MealServiceTest {
         // sans id car pas persistes
         childDto = new ChildDto("Dina", "CM2");
         childDto2 = new ChildDto("Tom", "CM2");
+        parentDto = new ParentDtoFull("Jacques","jc@toto.com");
         // avec id car persistes avec save
+        parentDto = parentService.saveParentWithChildId(parentDto,childAvecId.getId());
+        parentService.saveParentWithChildId(parentDto,childAvecId2.getId());
         childAvecId = childService.saveChild(childDto);
         childAvecId2 = childService.saveChild(childDto2);
         dateString = "2018-08-30";
@@ -50,12 +61,11 @@ public class MealServiceTest {
     public void tearDown() throws Exception {
         childService.deleteChildren(childAvecId.getId());
         childService.deleteChildren(childAvecId2.getId());
+        parentService.displayParentById(parentDto.getId());
     }
 
     @Test
     public void getAllMeals() {
-
-        Date day = new Date();
 
         MealDto m1 = mealService.saveMealNoDto(childAvecId.getId(), dateString);
         MealDto m2 = mealService.saveMealNoDto(childAvecId2.getId(), dateString);
@@ -73,8 +83,6 @@ public class MealServiceTest {
     @Test
     public void saveMealNoDto() {
 
-        Date day = new Date();
-
         MealDto m1 = mealService.saveMealNoDto(childAvecId.getId(), dateString);
         MealDto m2 = mealService.saveMealNoDto(childAvecId2.getId(), dateString);
 
@@ -87,8 +95,6 @@ public class MealServiceTest {
     @Test
     public void deleteMealById() {
 
-        Date day = new Date();
-
         MealDto m1 = mealService.saveMealNoDto(childAvecId.getId(), dateString);
         MealDto m2 = mealService.saveMealNoDto(childAvecId2.getId(), dateString);
 
@@ -100,13 +106,9 @@ public class MealServiceTest {
 
     @Test
     public void getMealsByDay() {
-//        Date day = new Date();
-//        Date day2 = new Date(day.getTime()+1000 * 60 * 60 * 25);
 
         String dateTest = "2018-05-25";
         String dateTest2 = "2018-05-24";
-
-//        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
         MealDto m1 = mealService.saveMealNoDto(childAvecId.getId(), dateTest);
         MealDto m2 = mealService.saveMealNoDto(childAvecId2.getId(), dateTest);
@@ -120,4 +122,14 @@ public class MealServiceTest {
         mealService.deleteMealById(m2.getId());
         mealService.deleteMealById(m3.getId());
     }
+    @Ignore
+    @Test
+    public void getMealsByParentId(){
+        MealDto m1 = mealService.saveMealNoDto(childAvecId.getId(), dateString);
+        MealDto m2 = mealService.saveMealNoDto(childAvecId2.getId(), dateString);
+        MealDto m3 = mealService.saveMealNoDto(childAvecId2.getId(), dateString);
+
+
+    }
+
 }

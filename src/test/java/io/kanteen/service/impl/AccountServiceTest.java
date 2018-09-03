809d2deb8cb95dc03bb37aabf7284645de72f065
Aleck Vincent <aleck.vincent@gmail.com>
2018-09-03 09:44:36 +0200

    removed setupIsDone and tearIsDone

--- src/test/java/io/kanteen/service/impl/AccountServiceTest.java
+++ src/test/java/io/kanteen/service/impl/AccountServiceTest.java
@@ -96,5 +96,7 @@ public class AccountServiceTest {
         service.deleteAccount(raymond.getId());
         service.deleteAccount(travis.getId());
         service.deleteAccount(kevin.getId());
+
+
     }
 }

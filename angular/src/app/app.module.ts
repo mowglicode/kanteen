f5448a6a8c02e8253798f9f1245e05c74919ef9f
Ambroise31 <testard31@gmail.com>
2018-09-03 11:09:26 +0200

    angular main tabs

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenusComponent } from './menus/menus.component';
 import {HttpClientModule} from "@angular/common/http";
+import { MainTabsComponent } from './main-tabs/main-tabs.component';
+import { MealsComponent } from './meals/meals.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenusComponent
+    MenusComponent,
+    MainTabsComponent,
+    MealsComponent
   ],
   imports: [
     BrowserModule, HttpClientModule

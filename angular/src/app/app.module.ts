9c760d2349d3a199135a21b828e68908174d8284
ripeisripe <rip_e@hotmail.com>
2018-09-04 15:04:59 +0200

    Merge branch 'dev' into manning
    # Conflicts:
    #	angular/src/app/app.component.html

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -7,13 +7,10 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import { MainTabsComponent } from './main-tabs/main-tabs.component';
 import { MealsComponent } from './meals/meals.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {
-  MatButtonModule,
   MatExpansionModule, MatIconModule,
   MatListModule,
   MatMenuModule,
-  MatTabsModule,
   MatToolbarModule
 } from "@angular/material";
 import { MenuAdminComponent } from './menus/menu-admin/menu-admin.component';

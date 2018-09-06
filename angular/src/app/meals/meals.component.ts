3d4aab23b1ee369fd117958f8e56bb691ff4bec6
ncolombie <nathalie.colombie@gmail.com>
2018-09-06 08:49:25 +0200

    picks pas finis. à faire: changer format date sauvegardée

--- angular/src/app/meals/meals.component.ts
+++ angular/src/app/meals/meals.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Child, MealsService} from "./meals.service";
+import {Child, MealsService, tickedChild} from "./meals.service";
 
 
 
@@ -44,6 +44,7 @@ export class MealsComponent implements OnInit {
 
   childSelection(event){
     console.log(event, event.source.value, event.checked);
+
     // childId prend la valeur de "value"(c.id) de l'event click une checkbox
     this.childId= event.source.value;
     //l'element de mealCheck avec la cle: event.source.value (=child id)
@@ -53,20 +54,28 @@ export class MealsComponent implements OnInit {
 
 
 
-    //deuxieme version pour gerer aussi de checker les checkbox qui correspondent déjà a un meal de la dataBase
+    //deuxieme version pour gérer aussi de checker les checkbox qui correspondent déjà à un meal de la dataBase
 
-    //childPickList= liste des childPick
+    //childPickList= liste des tickedChild
     let tickedChildList = this.service.tickedChildList;
-    // recupère le childPick (type ChildPick) qui a le child qui a l'id égale au child id de la checkbox qui est checked
-    let tickedChild = tickedChildList.find(childPick => childPick.child.id === event.source.value );
+
+    // recupère le tickedChild (type TickedChild) qui a le child qui a l'id égale au child id de la checkbox qui est checked
+    let tickedChild = tickedChildList.find(tickedChild => tickedChild.child.id === event.source.value );
+
     // attribue la valeur true ou false en fonction de si la checkbox est checked au booleen picked du ChildPick childPick
     tickedChild.ticked = event.checked;
+
+
     console.log(tickedChildList);
     console.log(tickedChild);
-    let  mealsDayChild = this.service.getMealsByParentId(this.service.loggedParentId)
+    console.log(tickedChild.child.name);//???? toujours le dernier clicke, checked ou non!!!
+    console.log(tickedChild.ticked);
+
+    //called when check a checkbox
+   /* let  mealsDayChild = this.service.getMealsByParentId(this.service.loggedParentId)
       .filter(function (meal){return meal.day;})
       .filter(function (meal) {return meal.child_id;})
-
+*/
   }
 
   /* Open when someone clicks on the span element */
@@ -78,7 +87,6 @@ export class MealsComponent implements OnInit {
   closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
-
 
 
   postMeal(){

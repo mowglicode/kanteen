c822a16a376ea6c73def70fd90028018247026fa
AaathenaaA <simonet.amandine@gmail.com>
2018-09-06 11:23:43 +0200

    add ContractAnswerServiceTest

---
+++ src/test/java/io/kanteen/service/impl/ContractAnswerServiceTest.java
@@ -0,0 +1,34 @@
+package io.kanteen.service.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContractAnswerServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void displayContractAnswersDto() {
+    }
+
+    @Test
+    public void displayContractAnswerDtoByContractId() {
+    }
+
+    @Test
+    public void deleteContractAnswer() {
+    }
+
+    @Test
+    public void saveContractAnswer() {
+    }
+}

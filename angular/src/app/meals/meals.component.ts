3406f49a7064217908dd219c147c0366b9e5dc79
Nicolas Zozol <nzozol@gmail.com>
2018-09-06 18:46:48 +0200

    Creating the ticker

--- angular/src/app/meals/meals.component.ts
+++ angular/src/app/meals/meals.component.ts
@@ -5,120 +5,126 @@ import {bind} from "../../../node_modules/@angular/core/src/render3/instructions
 
 
 @Component({
-  selector: 'app-meals',
-  templateUrl: './meals.component.html',
-  styleUrls: ['./meals.component.css']
+    selector: 'app-meals',
+    templateUrl: './meals.component.html',
+    styleUrls: ['./meals.component.css']
 })
 export class MealsComponent implements OnInit {
 
-  //checked=false;
+    //checked=false;
 
-  @Input() child: Child
-
-
-  childId: number = undefined;
-  // mealCheck= object = map  key value de type any
-  mealCheck: any = {}
-
-  idParentLogged:number = this.service.getParentByEmail(this.service.mailLogged).id;
-
-  constructor(public service: MealsService) {
-    this.service.getEatableDay();
-    this.service.getTickedChildListByParent(this.service.loggedParentId);
-    this.service.getMealsByParentId(this.service.loggedParentId);
-  }
-
-  ngOnInit() {
-  }
-
-  getLabelDay(i:number):string{
-    var d = new Date();
-    var weekday = new Array(7);
-    weekday[0] =  "Dimanche";
-    weekday[1] = "Lundi";
-    weekday[2] = "Mardi";
-    weekday[3] = "Mercredi";
-    weekday[4] = "Jeudi";
-    weekday[5] = "Vendredi";
-    weekday[6] = "Samedi";
-
-    return weekday[i];
-  }
-
-  format(day: string):string {
-    let date = new Date();
-    date.setFullYear(parseInt(day.slice(0, 4)));
-    date.setUTCMonth(parseInt(day.slice(5, 7)) - 1); // Month starts at 0 !
-    date.setUTCDate(parseInt(day.slice(8, 11))); // day of the month, starts at 1
-    // console.log(date);
-    return `${this.getLabelDay(date.getDay())} ${date.getDate()}-${date.getMonth() + 1}  `;
-  }
-
-  tabSelection(event){
-
-    console.log(event,"tab index=", event.index);
-    this.service.activeDay=this.service.eatableDay[event.index];
-    console.log("#Activeday#####"+this.service.activeDay);
-
-    let tickedChildList= this.service.tickedChildList;
-   /* tickedChildList. map( function (tickedChild){
-      tickedChild.day=this.activeDay;
-      return tickedChild}, bind(this));
-    */
-  }
-
-  childSelection(event) {
-    console.log(event, event.source.value, event.checked);
-   // this.activeDay=event.source.name;
-   // console.log("#Activeday?"+this.activeDay);
-
-    // childId prend la valeur de "value"(c.id) de l'event click une checkbox
-    this.childId = event.source.value;
-    //l'element de mealCheck avec la cle: event.source.value (=child id)
-    // prend la valeur true ou false qui depend de l'évenement click la checkbox (si elle est checked ou pas)
-    this.mealCheck[event.source.value] = event.checked;//???question nico
-    console.log(this.mealCheck);
+    @Input() child: Child
 
 
-    //deuxieme version pour gérer aussi de checker les checkbox qui correspondent déjà à un meal de la dataBase
-
-    //childPickList= liste des tickedChild
-    let tickedChildList = this.service.tickedChildList;
-
-    // recupère le tickedChild (type TickedChild) qui a le child qui a l'id égale au child id de la checkbox qui est checked
-    let tickedChild = tickedChildList.find(tickedChild => tickedChild.child.id === event.source.value);
-
-    // attribue la valeur true ou false en fonction de si la checkbox est checked au booleen picked du ChildPick childPick
-    tickedChild.ticked = event.checked;
-    tickedChild.day= this.service.activeDay;
+    childId: number = undefined;
+    // mealCheck= object = map  key value de type any
+    mealCheck: any = {}
 
 
-    console.log(tickedChildList);
-    console.log("tickedChild=",tickedChild);
-    console.log("childname=",tickedChild.child.name);//???? toujours le dernier clicke, checked ou non!!!
-    console.log("tiked?=",tickedChild.ticked);
+    constructor(public service: MealsService) {
+        /**
+         * Getting first children,
+         * then all meals,
+         * then eatable days
+         * then constructing the ticker
+         */
+        this.service.getChildrenByParent(this.service.loggedParentId)
+            .then(() => this.service.getMealsByParentId(this.service.loggedParentId))
+            .then(() => this.service.getEatableDay())
+            .then(() => this.service.createTicker())
 
-    //called when check a checkbox
-    /* let  mealsDayChild = this.service.getMealsByParentId(this.service.loggedParentId)
-       .filter(function (meal){return meal.day;})
-       .filter(function (meal) {return meal.child_id;})
- */
-  }
+    }
 
-  /* Open when someone clicks on the span element */
-  openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
+    ngOnInit() {
+    }
 
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+    getLabelDay(i: number): string {
+        var d = new Date();
+        var weekday = new Array(7);
+        weekday[0] = "Dimanche";
+        weekday[1] = "Lundi";
+        weekday[2] = "Mardi";
+        weekday[3] = "Mercredi";
+        weekday[4] = "Jeudi";
+        weekday[5] = "Vendredi";
+        weekday[6] = "Samedi";
+
+        return weekday[i];
+    }
+
+    format(day: string): string {
+        let date = new Date();
+        date.setFullYear(parseInt(day.slice(0, 4)));
+        date.setUTCMonth(parseInt(day.slice(5, 7)) - 1); // Month starts at 0 !
+        date.setUTCDate(parseInt(day.slice(8, 11))); // day of the month, starts at 1
+        // console.log(date);
+        return `${this.getLabelDay(date.getDay())} ${date.getDate()}-${date.getMonth() + 1}  `;
+    }
+
+    tabSelection(event) {
+
+        console.log(event, "tab index=", event.index);
+
+        console.log("#Activeday#####" + this.service.activeDay);
+
+        let tickedChildList = this.service.tickedChildList;
+        /* tickedChildList. map( function (tickedChild){
+           tickedChild.day=this.activeDay;
+           return tickedChild}, bind(this));
+         */
+    }
+
+    childSelection(event) {
+        console.log(event, event.source.value, event.checked);
+        // this.activeDay=event.source.name;
+        // console.log("#Activeday?"+this.activeDay);
+
+        // childId prend la valeur de "value"(c.id) de l'event click une checkbox
+        this.childId = event.source.value;
+        //l'element de mealCheck avec la cle: event.source.value (=child id)
+        // prend la valeur true ou false qui depend de l'évenement click la checkbox (si elle est checked ou pas)
+        this.mealCheck[event.source.value] = event.checked;//???question nico
+        console.log(this.mealCheck);
 
 
-  postMeal() {
-    this.service.saveMeal(this.childId, this.service.activeDay);
-    console.log('xxx', this.service.activeDay);
-  }
+        //deuxieme version pour gérer aussi de checker les checkbox qui correspondent déjà à un meal de la dataBase
+
+        //childPickList= liste des tickedChild
+        let tickedChildList = this.service.tickedChildList;
+
+        // recupère le tickedChild (type TickedChild) qui a le child qui a l'id égale au child id de la checkbox qui est checked
+        let tickedChild = tickedChildList.find(tickedChild => tickedChild.child.id === event.source.value);
+
+        // attribue la valeur true ou false en fonction de si la checkbox est checked au booleen picked du ChildPick childPick
+        tickedChild.ticked = event.checked;
+
+
+        console.log(tickedChildList);
+        console.log("tickedChild=", tickedChild);
+        console.log("childname=", tickedChild.child.name);//???? toujours le dernier clicke, checked ou non!!!
+        console.log("tiked?=", tickedChild.ticked);
+
+        //called when check a checkbox
+        /* let  mealsDayChild = this.service.getMealsByParentId(this.service.loggedParentId)
+           .filter(function (meal){return meal.day;})
+           .filter(function (meal) {return meal.child_id;})
+     */
+    }
+
+    /* Open when someone clicks on the span element */
+    openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    /* Close when someone clicks on the "x" symbol inside the overlay */
+    closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+
+
+    postMeal() {
+        this.service.saveMeal(this.childId, this.service.activeDay);
+        console.log('xxx', this.service.activeDay);
+    }
 
 }
--- angular/src/app/meals/meals.service.ts
+++ angular/src/app/meals/meals.service.ts
@@ -3,143 +3,224 @@ import {HttpClient} from "@angular/common/http";
 import {forEach} from "../../../node_modules/@angular/router/src/utils/collection";
 
 import {LoginService} from "../login.service";
+import {tick} from "@angular/core/testing";
 
 export interface Parent {
-  id: number;
-  name: string;
-  account?: string;
-  children: Array<Child>;
-  school?: string;
+    id: number;
+    name: string;
+    account?: string;
+    children: Array<Child>;
+    school?: string;
 }
 
 export interface Child {
-  id: number;
-  name: string;
-  grade: string;
+    id: number;
+    name: string;
+    grade: string;
 }
 
 //// Like a Meal without a date ======= CheckedChild ou tickedChild ???Like a Meal without a date
 export interface TickedChild {
-  child: Child;
-  ticked: boolean;
-  day: string;
+    child: Child;
+    ticked: boolean;
+}
+
+// For ech day, we have many Ticks available: one per child
+export interface TicksByDay {
+    day: string,
+    ticks: TickedChild[]
 }
 
 
 export interface Meal {
-  id: number;
-  day: string;
-  child: Child;
+    id: number;
+    day: string;
+    child: Child;
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MealsService {
 
-  constructor(public http: HttpClient, public loginService: LoginService) {
-  }
+    constructor(public http: HttpClient, public loginService: LoginService) {
+    }
 
-  activeDay: string;
-  mailLogged: string = this.loginService.mailLogged;
-  parentLogged:Parent = undefined;
-  eatableDay: string[] = [];
-  childrenByParent: Child[] = [];
-  loggedParentId: number = 1;
-  //list des tickedChild
-  tickedChildList: TickedChild[] = [];
-  mealsParent: Meal[] = [];
-  ticked: boolean = false;
+    // activeDay is needed for POSTING a new Meal
+    activeDay: string;
 
+    mailLogged: string = this.loginService.mailLogged;
+    parentLogged: Parent = undefined;
+    eatableDay: string[] = [];
+    childrenByParent: Child[] = [];
+    loggedParentId: number = 1;
+    //list des tickedChild
+    tickedChildList: TickedChild[] = [];
 
-  // Should have something with Meal and Date
-
-  getEatableDay() {
-    this.http.get('http://localhost:8585/api/dates/eatableday')
-      .subscribe((r: any[]) => {
-        this.eatableDay = r;
-        console.log('Eateable day', this.eatableDay);
-      });
-  }
-
-  getTickedChildListByParent(id) {
-    this.http.get('http://localhost:8585/api/children/parent/' + id)
-      .subscribe((r: any[]) => {
-        this.childrenByParent = r;
-        console.log('Childrenbyparent', this.childrenByParent);
-        this.tickedChildList = this.childrenByParent.map(mapChildByChildPick)
-          .map(childTick => {
+    ticker: TicksByDay[] = [];
+    mealsParent: Meal[] = [];
+    ticked: boolean = false;
 
 
-          })
-          .map(function (tickedChild) {
+    // Should have something with Meal and Date
+
+    getEatableDay() {
+        return this.http.get('http://localhost:8585/api/dates/eatableday')
+            .toPromise()
+            .then((r: any[]) => {
+                this.eatableDay = r;
+                console.log('Eateable day', this.eatableDay);
+                return this.eatableDay;
+            })
+    }
+
+    createTicker():TicksByDay[]{
+
+        if (this.eatableDay.length === 0){
+            console.error('no eatable day');
+            return []
+        }
+        if (this.childrenByParent.length === 0){
+            console.error('no child for parent '+this.loggedParentId);
+            return []
+        }
+
+        this.ticker = this.eatableDay.map(getDayByTickMapper(this.childrenByParent));
+
+        // Now check who is false or not
+        fillTickerWithMeals(this.ticker, this.mealsParent);
+
+        console.log('Ticker created : ', this.ticker);
+        return this.ticker;
+    }
+
+    getChildrenByParent(parentId) {
+        return this.http.get<Child[]>('http://localhost:8585/api/children/parent/' + parentId)
+            .toPromise()
+            .then(r =>this.childrenByParent = r)
+    }
+
+    getTickedChildListByParent(id) {
+        return this.http.get('http://localhost:8585/api/children/parent/' + id)
+            .toPromise()
+            .then((r: any[]) => {
+                this.childrenByParent = r;
 
 
-            if (this.isRetired(tickedChild.child.id, tickedChild.day)) {
-              tickedChild.ticked = true
-            }
-            console.log("mapped", tickedChild);
-            return tickedChild;
-          }.bind(this));
+                this.tickedChildList = this.childrenByParent.map(mapChildByChildPick)
+                    .map(childTick => {
 
 
-      })
-  }
+                    })
+                    .map(function (tickedChild) {
 
-  getParentByEmail(email: string):Parent {
-    this.http.get('http://localhost:8585/api/parents/email' + email)
-      .subscribe((r: any) => {
-        console.log("parent logged :"+r.name);
-        this.parentLogged=r;
-      })
-    return
-  }
 
+                        if (this.isRetired(tickedChild.child.id, tickedChild.day)) {
+                            tickedChild.ticked = true
+                        }
+                        console.log("mapped", tickedChild);
+                        return tickedChild;
+                    }.bind(this));
+
+
+            })
+    }
+
+    getParentByEmail(email: string) {
+        return this.http.get('http://localhost:8585/api/parents/email' + email)
+            .toPromise()
+            .then((r: any) => {
+                console.log("parent logged :" + r.name);
+                this.parentLogged = r;
+            })
+    }
 
 
 // Not the complete (good) api yet: need to check the meals present in the DB
-  // for each case, is it retired or not -> isRetired()
+    // for each case, is it retired or not -> isRetired()
+
+
+    saveMeal(childId, activeDay) {
+        this.http.post(`http://localhost:8585/api/meals/${childId}/${activeDay}`, null)
+            .subscribe();
+    }
+
+    getMealsByParentId(id) {
+        return this.http.get('http://localhost:8585/api/meals/parent/' + id)
+            .toPromise()
+            .then((r: any[]) => {
+
+                this.mealsParent = r;
+                console.log('mealsparent', this.mealsParent);
+            })
+    }
+
+
+    getRetiredChildrenNames() {
+        return this.tickedChildList.filter(tickedChild => tickedChild.ticked)
+            .map(c => c.child.name)
+    }
+
+    isRetired(childId: number, day: string) {
+        this.mealsParent.forEach(function (meal) {
+            if (meal.child.id === childId && meal.day === day) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+    }
+
+
+}
 
 
 
 
-  saveMeal(childId, activeDay) {
-    this.http.post(`http://localhost:8585/api/meals/${childId}/${activeDay}`, null)
-      .subscribe();
-  }
+function getDayByTickMapper(children: Child[]) {
 
-  getMealsByParentId(id) {
-    this.http.get('http://localhost:8585/api/meals/parent/' + id)
-      .subscribe((r: any[]) => {
-        this.mealsParent = r;
-        console.log('mealsparent', this.mealsParent);
-      })
-    return this.mealsParent;
-  }
+    // this mapper depends on parent children
+    return function mapDayByTick(day: string): TicksByDay {
+
+        return {
+            day,
+            ticks: children.map(child => ({
+                child,
+                ticked: false
+            }))
+        }
+
+    }
+
+}
 
 
-  getRetiredChildrenNames() {
-    return this.tickedChildList.filter(tickedChild => tickedChild.ticked)
-      .map(c => c.child.name)
-  }
+/**
+ * Mutative function that will go into the tree
+ * and eventually update ticker.ticks[].ticked to true
+ */
+function fillTickerWithMeals(ticker:TicksByDay[], meals:Meal[]){
 
-  isRetired(childId: number, day: string) {
-    this.mealsParent.forEach(function (meal) {
-      if (meal.child.id === childId && meal.day === day) {
-        return true;
-      } else {
-        return false;
-      }
+    ticker.forEach( (ticksByDay:TicksByDay) =>  {
+
+        meals.forEach(meal => {
+            if (meal.day === ticksByDay.day ){
+                ticksByDay.ticks.forEach(tick => {
+                    if (tick.child.id === meal.child.id){
+                        tick.ticked = true;
+                    }
+                })
+
+            }
+        })
     })
-  }
-
 
 }
 
 
 function mapChildByChildPick(child, day) {
-  return {
-    child,
-    ticked: false
-  }
+    return {
+        child,
+        ticked: false
+    }
 }

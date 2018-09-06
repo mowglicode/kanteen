07194ff6558b4b41ece4793471a5d51aadb778ec
Ambroise31 <testard31@gmail.com>
2018-09-06 10:42:09 +0200

    Clean stuff

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -19,18 +19,14 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(this.user.email);
-    console.log(this.user.password);
     this.service.checkLoginStatus(this.user.email, this.user.password);
   }
 
   isAdmin(){
-    console.log("Admin :" + this.service.isAdmin);
     return this.service.isAdmin;
   }
 
   isLogged(){
-    console.log("Loggué"+this.service.isLogged);
     return this.service.isLogged;
   }
 

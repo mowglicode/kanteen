3002cbfe9bec4dfe47c6dbeaf91569258cd3b9b3
ripeisripe <rip_e@hotmail.com>
2018-09-07 10:40:38 +0200

    I added the work of people and Xavier for form

--- angular/src/app/admin/admin-home/admin-home.service.ts
+++ angular/src/app/admin/admin-home/admin-home.service.ts
@@ -1,60 +1,148 @@
 import { Injectable } from '@angular/core';
+
 import {HttpClient} from "@angular/common/http";
 
+import {any} from "../../../../node_modules/codelyzer/util/function";
+
+
+
 export type Information = {
+
   id:number
+
   description:string
+
   expiry:string
+
   hasExpiration:boolean
+
 }
+
 @Injectable({
+
   providedIn: 'root'
+
 })
+
 export class AdminHomeService {
 
+
+
   informations: Information[] = [];
+
   notExpiredInformations: Information[] = [];
+
   information: Information;
 
 
 
+
+
+
+
   constructor(public http: HttpClient) {
+
   }
+
+
 
   informationUrl = 'http://localhost:8585/api/informations';
 
+
+
   informationUrlToDelete = 'http://localhost:8585/api/informations/';
 
+
+
   getInformation() {
+
     return this.http.get(this.informationUrl);
+
   }
 
+
+
   hasExpired(expiry:string) {
+
     let expiryDate=new Date();
+
     expiryDate.setFullYear(parseInt(expiry.slice(0,4)));
+
     expiryDate.setUTCMonth(parseInt(expiry.slice(5,7)) -1 ); // Month starts at 0 !
+
     expiryDate.setUTCDate(parseInt(expiry.slice(8,11))); // day of the month, starts at 1
+
+
 
     let now = new Date();
 
+
+
     return expiryDate.getTime() - now.getTime() < -1;
+
   }
 
+
+
   getAllInformations() {
+
     this.http.get(this.informationUrl)
+
       .subscribe((result: any[]) => {
+
         this.informations = result
+
         this.treatInformations();
+
       });
+
   }
+
+
+
 
 
   treatInformations() {
+
     this.informations.forEach(info => {
+
       if (!this.hasExpired(info.expiry)){
+
         this.notExpiredInformations.push(info);
+
       }
+
     })
+
   }
+
+
+
+  delete(id) {
+
+    this.http.delete(this.informationUrlToDelete + id)
+
+      .subscribe((result: any[]) => {
+
+      });
+
+  }
+
+
+
+  addInformation(i) {
+
+    // i.expiry
+
+    this.http.post(this.informationUrl, i)
+
+      .subscribe((result => {
+
+        console.log(result);
+
+      }));
+
+  }
+
 }
 
